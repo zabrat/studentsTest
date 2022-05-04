9bf00218dcda268e4fc75c0e1a94f437d970edde
Macbook <macbook2020@172-10-190-166.lightspeed.gnvlsc.sbcglobal.net>
2022-05-04 17:21:21 +0400

    done

--- src/Controller/controller.mjs
+++ src/Controller/controller.mjs
@@ -4,5 +4,39 @@ export default class Controller {
     this.model = model;
   }
 
-  init() {}
+  init() {
+    this.render();
+  }
+  render() {
+    // sign.addEventListener("keydown", function (event) {
+    //   if (event.keyCode === 107) {
+    //     sumNum();
+    //   }
+    // });
+    this.view.firstButton.addEventListener("click", (event) => {
+      this.view.firstSpan.innerText = event.target.value;
+      this.view.secondSpan.innerText =
+        Number(this.view.first_num.value) + Number(this.view.second_num.value);
+    });
+    this.view.secondButton.addEventListener("click", (event) => {
+      this.view.firstSpan.innerText = event.target.value;
+      this.view.secondSpan.innerText =
+        Number(this.view.first_num.value) - Number(this.view.second_num.value);
+    });
+    this.view.threeButton.addEventListener("click", (event) => {
+      this.view.firstSpan.innerText = event.target.value;
+      this.view.secondSpan.innerText =
+        Number(this.view.first_num.value) * Number(this.view.second_num.value);
+    });
+    this.view.forthButton.addEventListener("click", (event) => {
+      this.view.firstSpan.innerText = event.target.value;
+      this.view.secondSpan.innerText =
+        Number(this.view.first_num.value) / Number(this.view.second_num.value);
+    });
+
+    // function sumNum() {
+    //   let sum = Number(first_num) + Number(second_num);
+    //   result.innerHTML = sum;
+    // }
+  }
 }
--- src/View/view.mjs
+++ src/View/view.mjs
@@ -1,3 +1,13 @@
 export default class View {
-  constructor() {}
+  constructor() {
+    this.first_num = document.getElementById("first_number");
+    this.second_num = document.getElementById("second_number");
+    this.result = document.getElementById("result");
+    this.firstButton = document.querySelector(".first-button");
+    this.secondButton = document.querySelector(".second-button");
+    this.threeButton = document.querySelector(".three-button");
+    this.forthButton = document.querySelector(".forth-button");
+    this.firstSpan = document.querySelector(".first-span");
+    this.secondSpan = document.querySelector(".second-span");
+  }
 }
