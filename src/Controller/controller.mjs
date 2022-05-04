a7dd201e7e1ec89fe4e5938b98ac7104ee41383a
Macbook <macbook2020@172-10-190-166.lightspeed.gnvlsc.sbcglobal.net>
2022-05-04 18:20:11 +0400

    final

--- src/Controller/controller.mjs
+++ src/Controller/controller.mjs
@@ -8,25 +8,42 @@ export default class Controller {
     this.render();
   }
   render() {
-    this.view.firstButton.addEventListener("click", (event) => {
-      this.view.firstSpan.innerText = event.target.value;
-      this.view.secondSpan.innerText =
-        Number(this.view.first_num.value) + Number(this.view.second_num.value);
+    this.view.first_num.addEventListener("keyup", (event) => {
+      this.model.firstNumber = +event.target.value;
     });
-    this.view.secondButton.addEventListener("click", (event) => {
-      this.view.firstSpan.innerText = event.target.value;
-      this.view.secondSpan.innerText =
-        Number(this.view.first_num.value) - Number(this.view.second_num.value);
+    this.view.second_num.addEventListener("keyup", (event) => {
+      this.model.secondNumber = +event.target.value;
     });
-    this.view.threeButton.addEventListener("click", (event) => {
-      this.view.firstSpan.innerText = event.target.value;
-      this.view.secondSpan.innerText =
-        Number(this.view.first_num.value) * Number(this.view.second_num.value);
-    });
-    this.view.forthButton.addEventListener("click", (event) => {
-      this.view.firstSpan.innerText = event.target.value;
-      this.view.secondSpan.innerText =
-        Number(this.view.first_num.value) / Number(this.view.second_num.value);
+
+    this.view.buttons.forEach((element) => {
+      element.addEventListener("click", (event) => {
+        this.model.currentOperator = event.target.value;
+        switch (event.target.value) {
+          case "+":
+            this.view.firstSpan.innerText = event.target.value;
+            this.view.secondSpan.innerText =
+              this.model.firstNumber + this.model.secondNumber;
+            this.model.result = this.view.secondSpan.innerText;
+            break;
+          case "-":
+            this.view.firstSpan.innerText = event.target.value;
+            this.view.secondSpan.innerText =
+              this.model.firstNumber - this.model.secondNumber;
+            this.model.result = this.view.secondSpan.innerText;
+            break;
+          case "/":
+            this.view.firstSpan.innerText = event.target.value;
+            this.view.secondSpan.innerText =
+              this.model.firstNumber / this.model.secondNumber;
+            this.model.result = this.view.secondSpan.innerText;
+            break;
+          case "*":
+            this.view.firstSpan.innerText = event.target.value;
+            this.view.secondSpan.innerText =
+              this.model.firstNumber * this.model.secondNumber;
+            this.model.result = this.view.secondSpan.innerText;
+        }
+      });
     });
   }
 }
--- src/View/view.mjs
+++ src/View/view.mjs
@@ -3,6 +3,7 @@ export default class View {
     this.first_num = document.getElementById("first_number");
     this.second_num = document.getElementById("second_number");
     this.result = document.getElementById("result");
+    this.buttons = document.querySelectorAll(".button");
     this.firstButton = document.querySelector(".first-button");
     this.secondButton = document.querySelector(".second-button");
     this.threeButton = document.querySelector(".three-button");
